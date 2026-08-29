64bb958bce880967740bb436a3541be4d0669679
coderDreams <1525308237@qq.com>
2026-08-29 15:01:40 +0800

    fix(dev): 修复 bat 模板反斜杠被 JS 转义吞掉的问题

--- dev-backend.example.bat
+++ dev-backend.example.bat
@@ -4,11 +4,11 @@ echo IMPORTANT: open tunnel.bat FIRST !
 echo.
 
 rem Change to your own IDEA JBR path
-set "JAVA_HOME=C:UsersadminAppDataLocalProgramsIntelliJ IDEAjbr"
+set "JAVA_HOME=C:\Users\admin\AppData\Local\Programs\IntelliJ IDEA\jbr"
 
 rem Dev database password: MYSQL_PASSWORD in /root/.db_credentials on server
 set "DEV_DB_PASSWORD=PUT_YOUR_PASSWORD_HERE"
 
-cd /d "D:Desktopagent_util_scriptscoderzhang-blogackend"
-mvn -s ..mvn-settings.xml spring-boot:run -Dspring-boot.run.profiles=dev
+cd /d "D:\Desktop\agent_util_scripts\coderzhang-blog\backend"
+mvn -s ..\mvn-settings.xml spring-boot:run -Dspring-boot.run.profiles=dev
 pause >nul
